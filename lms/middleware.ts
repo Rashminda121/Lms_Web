82f2c25a658f2592ad8cbef4299a491fa16ef13f
Jayamuni Rashminda <rashminda121@gmail.com>
2024-06-21 07:39:49 +0530

    added side bar

--- lms/middleware.ts
+++ lms/middleware.ts
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+// export default clerkMiddleware();
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
 
-export default clerkMiddleware();
+// export default authMiddleware({
+//   // publicRoutes: ["/test"],
+// });
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
