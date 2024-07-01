ef5cfcf2da02235e63487620d43e9713ea96ce12
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-01 20:44:08 +0530

    chapter video

--- lms/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ lms/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,12 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { Mux } from "@mux/mux-node";
-
-const { Video } = new Mux(
-  process.env.MUX_TOKEN_ID!,
-  process.env.MUX_TOKEN_SECRET!
-);
+import Mux from "@mux/mux-node";
 
 export async function PATCH(
   req: Request,
