87c2590f85b897e4438e35e313403d94d4eecb69
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-06 15:27:20 +0530

    chapter video play

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -5,15 +5,14 @@ import * as z from "zod";
 
 import { FileUpload } from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
-import { Chapter, MuxData } from "@prisma/client";
 import { Pencil, PlusCircle, Video } from "lucide-react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import ReactPlayer from "react-player";
 
 interface ChapterVideoFormProps {
-  initialData: Chapter & { muxData?: MuxData | null };
+  initialData: { videoUrl: string | null };
   courseId: string;
   chapterId: string;
 }
@@ -28,6 +27,7 @@ export const ChapterVideoForm = ({
   chapterId,
 }: ChapterVideoFormProps) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [videoUrl, setVideoUrl] = useState(initialData.videoUrl);
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
@@ -41,6 +41,7 @@ export const ChapterVideoForm = ({
       );
       toast.success("Chapter Updated");
       toggleEdit();
+      setVideoUrl(values.videoUrl);
       router.refresh();
     } catch {
       toast.error("Something went wrong");
@@ -53,13 +54,13 @@ export const ChapterVideoForm = ({
         Chapter video
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
-          {!isEditing && !initialData.videoUrl && (
+          {!isEditing && !videoUrl && (
             <>
               <PlusCircle className="h-4 w-4 mr-2" />
-              Add an video
+              Add a video
             </>
           )}
-          {!isEditing && initialData.videoUrl && (
+          {!isEditing && videoUrl && (
             <>
               <Pencil className="h-4 w-4 mr-2" />
               Edit video
@@ -68,12 +69,14 @@ export const ChapterVideoForm = ({
         </Button>
       </div>
       {!isEditing &&
-        (!initialData.videoUrl ? (
+        (!videoUrl ? (
           <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
             <Video className="h-10 w-10 text-slate-500" />
           </div>
         ) : (
-          <div className="relative aspect-video mt-2">Video uploaded!</div>
+          <div className="relative aspect-video mt-2">
+            <ReactPlayer url={videoUrl} controls width="100%" height="100%" />
+          </div>
         ))}
       {isEditing && (
         <div>
@@ -90,12 +93,116 @@ export const ChapterVideoForm = ({
           </div>
         </div>
       )}
-      {initialData.videoUrl && !isEditing && (
+      {videoUrl && !isEditing && (
         <div className="text-xs text-muted-foreground mt-2">
-          Vdeos can take a few minutes to process. Refresh the page if video
-          does not appear.
+          Videos can take a few minutes to process. Refresh the page if the
+          video does not appear.
         </div>
       )}
     </div>
   );
 };
+
+// "use client";
+
+// import axios from "axios";
+// import * as z from "zod";
+
+// import { FileUpload } from "@/components/file-upload";
+// import { Button } from "@/components/ui/button";
+// import { Chapter, MuxData } from "@prisma/client";
+// import { Pencil, PlusCircle, Video } from "lucide-react";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+// import MuxPlayer from "@mux/mux-player-react";
+
+// interface ChapterVideoFormProps {
+//   initialData: Chapter & { muxData?: MuxData | null };
+//   courseId: string;
+//   chapterId: string;
+// }
+
+// const formSchema = z.object({
+//   videoUrl: z.string().min(1),
+// });
+
+// export const ChapterVideoForm = ({
+//   initialData,
+//   courseId,
+//   chapterId,
+// }: ChapterVideoFormProps) => {
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   const toggleEdit = () => setIsEditing((current) => !current);
+
+//   const router = useRouter();
+
+//   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+//     try {
+//       await axios.patch(
+//         `/api/courses/${courseId}/chapters/${chapterId}`,
+//         values
+//       );
+//       toast.success("Chapter Updated");
+//       toggleEdit();
+//       router.refresh();
+//     } catch {
+//       toast.error("Something went wrong");
+//     }
+//   };
+
+//   return (
+//     <div className="mt-6 border bg-slate-100 rounded-md p-4">
+//       <div className="font-medium flex items-center justify-between">
+//         Chapter video
+//         <Button onClick={toggleEdit} variant="ghost">
+//           {isEditing && <>Cancel</>}
+//           {!isEditing && !initialData.videoUrl && (
+//             <>
+//               <PlusCircle className="h-4 w-4 mr-2" />
+//               Add an video
+//             </>
+//           )}
+//           {!isEditing && initialData.videoUrl && (
+//             <>
+//               <Pencil className="h-4 w-4 mr-2" />
+//               Edit video
+//             </>
+//           )}
+//         </Button>
+//       </div>
+//       {!isEditing &&
+//         (!initialData.videoUrl ? (
+//           <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+//             <Video className="h-10 w-10 text-slate-500" />
+//           </div>
+//         ) : (
+//           <div className="relative aspect-video mt-2">
+//             <MuxPlayer playbackId={initialData?.muxData?.playbackId || ""} />
+//           </div>
+//         ))}
+//       {isEditing && (
+//         <div>
+//           <FileUpload
+//             endpoint="chapterVideo"
+//             onChange={(url) => {
+//               if (url) {
+//                 onSubmit({ videoUrl: url });
+//               }
+//             }}
+//           />
+//           <div className="text-xs text-muted-foreground mt-4">
+//             Upload this chapter&apos;s video
+//           </div>
+//         </div>
+//       )}
+//       {initialData.videoUrl && !isEditing && (
+//         <div className="text-xs text-muted-foreground mt-2">
+//           Vdeos can take a few minutes to process. Refresh the page if video
+//           does not appear.
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
