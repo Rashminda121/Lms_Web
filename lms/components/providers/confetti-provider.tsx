c259efb85a6ac465bb2fbe934949547a92872a0d
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-08 19:18:33 +0530

    confetti update

--- lms/components/providers/confetti-provider.tsx
+++ lms/components/providers/confetti-provider.tsx
@@ -2,16 +2,11 @@
 
 import { ReactConfetti } from "react-confetti";
 
-
 import { useConfettiStore } from "@/hooks/use-confetti-store";
 
-export const ConfettiProvider=()=>{
-    const confetti=useConfettiStore();
+export const ConfettiProvider = () => {
+  const confetti = useConfettiStore();
 
-    if(!confetti.isOpen){
-        
-    }
-    if(!confetti.isOpen){
-        
-    }
-}
+  if (!confetti.isOpen) {
+  }
+};
