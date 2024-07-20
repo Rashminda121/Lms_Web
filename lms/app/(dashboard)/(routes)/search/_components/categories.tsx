da8a726f41c9631e4181c6da1f0bacb8cf95ba99
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-20 05:53:44 +0530

    browse page

---
+++ lms/app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Category } from "@prisma/client";
+
+interface CategoriesProps {
+  items: Category[];
+}
+
+export const Categories = ({ items }: CategoriesProps) => {
+  return <div>Categories!</div>;
+};
