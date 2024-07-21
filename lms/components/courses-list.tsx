13a89cb57e2f7f0b0ba52e73a70da1f2322b50d2
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-21 18:00:33 +0530

    courses list update

--- lms/components/courses-list.tsx
+++ lms/components/courses-list.tsx
@@ -14,7 +14,6 @@ export const CoursesList = ({ items }: CoursesListProps) => {
     <div>
       {items.map((item) => (
         <div key={item.id}>{item.title}</div>
-        <div key={item.id}>{item.title}</div>
       ))}
     </div>
   );
