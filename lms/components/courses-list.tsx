1ca34a2b97c7bf4b72fbd7ca2bcfc8a9f8f0db62
Jayamuni Rashminda <rashminda121@gmail.com>
2024-07-26 16:18:05 +0530

    courses list display

--- lms/components/courses-list.tsx
+++ lms/components/courses-list.tsx
@@ -12,9 +12,16 @@ interface CoursesListProps {
 export const CoursesList = ({ items }: CoursesListProps) => {
   return (
     <div>
-      {items.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+      <div>
+        {items.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center text-sm text-muted-foreground mt-10">
+          No courses found
+        </div>
+      )}
     </div>
   );
 };
